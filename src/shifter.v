815c2e1c407b70be8942acf6afc78b7125eb7067
Abdelsalam ElTamawy <40343437+solomspd@users.noreply.github.com>
2019-10-23 18:11:49 +0200

    fixed shifter

--- src/shifter.v
+++ src/shifter.v
@@ -1,11 +1,11 @@
-module shifter (input [31:0]in, input [4:0]shamt, input [1:0]alu, output [31:0]out);
+module shifter (input [31:0]in, input [4:0]shamt, input [1:0]alu, output reg [31:0]out);
 
 always @(*) begin
-    case (alu) begin
+    case (alu)
         2'b00: out = in << shamt;
         2'b01: out = in >>> shamt;
         2'b10: out = in >> shamt;
-    end
+    endcase
 end
 
 endmodule
