ce6a6379bf4d77be761bd2d99d5ec35889430ecf
Abdelsalam ElTamawy <40343437+solomspd@users.noreply.github.com>
2019-10-23 18:10:56 +0200

    fixed branch module

--- src/branch.v
+++ src/branch.v
@@ -1,16 +1,16 @@
 `include "defines.v"
 
-module branch (input [2:0]func3, input zero, input carry, input overflow, input sign, output out);
+module branch (input [2:0]func3, input zero, input carry, input overflow, input sign, output reg out);
 
 always @(*) begin
-    case (func3) begin
+    case (func3)
         `BR_BEQ: out = zero;
         `BR_BNE: out = ~zero;
         `BR_BLT: out = (sign != overflow);
         `BR_BGE: out = (sign == overflow);
         `BR_BLTU: out = ~carry;
         `BR_BGEU: out = carry;
-    end
+    endcase
 end
 
 endmodule
