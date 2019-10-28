b9ed9dc67df586d0d56bab959d5ae23c3595994d
Rinal-M <52177292+Rinal-M@users.noreply.github.com>
2019-10-29 01:32:57 +0200

    comments added to the mux module

--- src/mux.v
+++ src/mux.v
@@ -1,23 +1,18 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/17/2019 03:07:59 PM
-// Design Name: 
-// Module Name: mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+/*******************************************************************
+*
+* Module: register.v
+* Project: RISC-V FPGA Implementation and Testing 
+* Author: 
+* Ahmed Ibrahim  ahmeddibrahim@aucegypt.edu
+* Abd El-Salam   solomspd@aucegypt.edu
+* Andrew Kamal   andrewk.kamal@aucegypt.edu
+* Rinal Mohamed  rinalmohamed@aucegypt.edu
+* Description: This module just creates a multiplexer module to *be used later in the data path whenever needed. We just *instantiate from that module and use it right away
+*
+* Change history: 09/17/2019 03:07:59 PM - Module created by Abd *El-Salam in the lab
+*
+**********************************************************************/
 
 module mux(input a, input b, input sel, output c);
     assign c = sel ? b : a;
