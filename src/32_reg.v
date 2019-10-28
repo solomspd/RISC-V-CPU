1ea3e4e7c300bbec79fea496ccf3ffa9182ea09e
Rinal-M <52177292+Rinal-M@users.noreply.github.com>
2019-10-29 01:29:23 +0200

    comments added to the register file module

--- src/32_reg.v
+++ src/32_reg.v
@@ -1,24 +1,20 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/17/2019 03:11:55 PM
-// Design Name: 
-// Module Name: 32_reg
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
+/*******************************************************************
+*
+* Module: register.v
+* Project: RISC-V FPGA Implementation and Testing 
+* Author: 
+* Ahmed Ibrahim  ahmeddibrahim@aucegypt.edu
+* Abd El-Salam   solomspd@aucegypt.edu
+* Andrew Kamal   andrewk.kamal@aucegypt.edu
+* Rinal Mohamed  rinalmohamed@aucegypt.edu
+* Description: This module just creates one instance of a *register to be used later in the registers file module to create *the 32 registers required for the processor
+*
+* Change history: 09/17/2019 03:07:59 PM - Module created by Abd *El-Salam in the lab
+* 10/24/19 – Ahmed, Andrew, Abd El-Salam, and Rinal worked on the *module to make it compatible with the other modules modified in *the sources files
+* 10/29/19- Ahmed, Andrew, Abd El-Salam, and Rinal made the final modificaions to the module
+*
+**********************************************************************/
 
 module register(input clk, input [31:0]in, input rst, input load, output [31:0]out);
     
