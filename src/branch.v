953ed38c58e3ac0ce1043a6bdebceaac57851f62
Rinal-M <52177292+Rinal-M@users.noreply.github.com>
2019-10-29 01:57:14 +0200

    comments added to the branch module

--- src/branch.v
+++ src/branch.v
@@ -1,4 +1,19 @@
 `include "defines.v"
+*******************************************************************
+*
+* Module: branch.v
+* Project: RISC-V FPGA Implementation and Testing 
+* Author: 
+* Ahmed Ibrahim  ahmeddibrahim@aucegypt.edu
+* Abd El-Salam   solomspd@aucegypt.edu
+* Andrew Kamal   andrewk.kamal@aucegypt.edu
+* Rinal Mohamed  rinalmohamed@aucegypt.edu
+* Description: This module was created to handle all the branch *instructions that will be handled by the processor
+*
+* Change history: 24/10/2019 - Module created and modified based *on the needs of each branch instruction
+*25/10/2019 - fixed branch module
+**********************************************************************/
+
 
 module branch (input [2:0]func3, input zero, input carry, input overflow, input sign, output reg out);
 
