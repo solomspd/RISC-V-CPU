baae113bb74280f19c65509efe0337a07544724c
unknown <solomspd>
2019-10-29 02:01:25 +0200

    data memory comments

--- src/data_mem.v
+++ src/data_mem.v
@@ -1,23 +1,20 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 26/10/2019 02:35:50 PM
-// Design Name:
-// Module Name: data_mem
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
+/*******************************************************************
+*
+* Module: register.v
+* Project: RISC-V FPGA Implementation and Testing 
+* Author: 
+* Ahmed Ibrahim  ahmeddibrahim@aucegypt.edu
+* Abd El-Salam   solomspd@aucegypt.edu
+* Andrew Kamal   andrewk.kamal@aucegypt.edu
+* Rinal Mohamed  rinalmohamed@aucegypt.edu
+* Description: This module just creates one instance of a *register to be used later in the registers file module to create *the 32 registers required for the processor
+*
+* Change history: 09/17/2019 03:07:59 PM - Module created by Abd *El-Salam in the lab
+* 26/10/19 - made memory byte addressable and added all instructions (sb,lb, etc..) 
+* 29/10/19 - made memory 4k bytes big
+*
+**********************************************************************/
 
 module DataMem (input clk, input MemRead, input MemWrite, input [5:0] addr,input [2:0]func3, input [31:0] data_in, output reg [31:0]  data_out);
     reg [7:0] mem [(4*1024-1):0]; 
